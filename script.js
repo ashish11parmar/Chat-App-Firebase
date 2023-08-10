c0df5c52b39eade01c80334676eb70d3362dfdd4
Ashish <parmarashish1105@gmail.com>
2023-08-10 09:53:48 +0530

    database mail change and login with google done

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
 import {getDatabase} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js"
-import {getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider}  from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js';
+import {getAuth, signInWithPopup, GoogleAuthProvider}  from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhxPL3MBL-SLYVGQl63M6k_UEVfuwROOs",
-  authDomain: "chat-app-e882d.firebaseapp.com",
-  projectId: "chat-app-e882d",
-  storageBucket: "chat-app-e882d.appspot.com",
-  messagingSenderId: "621453985230",
-  appId: "1:621453985230:web:cf6feba1e1ca77e665179f",
-  measurementId: "G-DDP8M5JZSD"
+  apiKey: "AIzaSyAl5Qn1CPrW50A3HBTLq1DbN25w7D9BM2Q",
+  authDomain: "chatapp-10dc8.firebaseapp.com",
+  projectId: "chatapp-10dc8",
+  storageBucket: "chatapp-10dc8.appspot.com",
+  messagingSenderId: "97253407161",
+  appId: "1:97253407161:web:5281c0de61547a9ba81e68",
+  measurementId: "G-5XVTRB2RBS"
 };
 
 // Initialize Firebase
@@ -35,14 +35,19 @@ Google.addEventListener('click', (e)=>{
     const token = credential.accessToken;
     // The signed-in user info.
     const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
+
+    location.href = ("http://localhost:5500/chatView.html");
+   
   }).catch((error) => {
     // Handle Errors here.
     const errorCode = error.code;
+    console.log(errorCode);
     const errorMessage = error.message;
+    alert(errorMessage);
     // The email of the user's account used.
     const email = error.customData.email;
+    alert(email)
+    location.href = ("http://localhost:5500/index.html");
     // The AuthCredential type that was used.
     const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
